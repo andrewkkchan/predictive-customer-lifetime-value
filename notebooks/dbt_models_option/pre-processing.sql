4dcc3117904646e78cbd3914fb174a67b1a54ce6
andrewkkchan <andrew.chan@blvd.co>
2025-02-15 16:26:17 +0000

    full tested snowflake SQL

--- notebooks/dbt_models_option/pre-processing.sql
+++ notebooks/dbt_models_option/pre-processing.sql
@@ -1,3 +1,26 @@
+USE DATABASE CLV_DB;
+USE WAREHOUSE CLV_WH;
+CREATE OR REPLACE TABLE my_transactions (
+    id VARCHAR,
+    chain VARCHAR,
+    dept VARCHAR,
+    category VARCHAR,
+    company VARCHAR,
+    brand VARCHAR,
+    date DATE,
+    productsize FLOAT,
+    productmeasure VARCHAR,
+    purchasequantity FLOAT,
+    purchaseamount FLOAT
+);
+
+COPY INTO my_transactions
+FROM @my_internal_stage/transactions_company_1078778070.csv
+FILE_FORMAT = (FORMAT_NAME = 'my_csv_format');
+
+select * from my_transactions limit 10;
+
+CREATE OR REPLACE TABLE customer_level_data AS
 WITH filtered AS (
   SELECT 
     id,
@@ -39,17 +62,18 @@ holdout_value AS (
   GROUP BY f.id
 ),
 calibration_attributes AS (
-  SELECT DISTINCT ON (f.id) 
+  SELECT 
     f.id,
     f.chain,
     f.dept,
     f.category,
     f.brand,
-    f.productmeasure
+    f.productmeasure,
+    ROW_NUMBER() OVER (PARTITION BY f.id ORDER BY f.purchaseamount DESC) AS rn
   FROM filtered f
   JOIN start_dates s ON f.id = s.id
   WHERE f.date_dt = s.start_date
-  ORDER BY f.id, f.purchaseamount DESC
+  QUALIFY rn = 1
 )
 SELECT 
   cv.id,
@@ -60,8 +84,10 @@ SELECT
   ca.category,
   ca.brand,
   ca.productmeasure,
-  LOG(cv.calibration_value) AS log_calibration_value,
+  LN(cv.calibration_value) AS log_calibration_value,
   COALESCE(hv.holdout_value, 0) AS label
 FROM calibration_value cv
 LEFT JOIN holdout_value hv ON cv.id = hv.id
 LEFT JOIN calibration_attributes ca ON cv.id = ca.id;
+
+select * from customer_level_data;
